7df3e4a142133019aa41d6a945a7e4b99ce8f321
Eddy <edkosyan2000@gmail.com>
2024-07-17 18:17:55 +0400

    add logout button

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,13 +1,26 @@
 import styled from "styled-components";
 import { TableView } from "../views/MainViews/TableView";
+import { Button } from "@mui/material";
+import { useDispatch } from "react-redux";
+import { setToken } from "../store/user/slice";
+import LogoutIcon from "@mui/icons-material/Logout";
 
 const StyledPage = styled.div`
   padding: 30px;
 `;
 
 export const MainPage = () => {
+  const dispatch = useDispatch();
+
   return (
     <StyledPage>
+      <Button
+        variant="outlined"
+        endIcon={<LogoutIcon />}
+        onClick={() => dispatch(setToken(null))}
+      >
+        Logout
+      </Button>
       <TableView />
     </StyledPage>
   );
